688010482fc5884b648d98f3c15acb226f5caab0
martingautier7@gmail.com <martingautier7@gmail.com>
2024-10-29 14:19:09 +0000

    margin per product

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,3 +1,18 @@
-SELECT orders_id,quantity
-FROM gz_raw_data.raw_gz_sales
-ORDER BY orders_id
+SELECT
+    date_date,
+    orders_id,
+    product.products_id,
+    quantity,
+    purchase_price,
+    revenue,    
+    (quantity * purchase_price) AS purchase_cost,
+    ROUND((revenue - (quantity * purchase_price)),2) AS margin
+FROM
+    {{ ref('stg_raw__product') }} AS product
+LEFT JOIN
+    {{ ref('stg_raw__sales') }} AS sales
+ON
+    product.products_id = sales.product_id
+
+
+
